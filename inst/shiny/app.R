6033f0935b6130f14e238f594d213fa0b9279134
Michael Silverstein <msilverstein@finchtherapeutics.com>
2021-02-09 13:21:27 -0500

    comment

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -4,7 +4,7 @@ library(shinyjs)
 source(file.path("utils", "helpers.R"),  local = TRUE)
 
 ui <- navbarPage(
-  #title = paste("BatchQC v", packageVersion("BatchQC"), sep = ""),
+  # title = paste("BatchQC v", packageVersion("BatchQC"), sep = ""),
   title = "BatchQC",
   id="BatchQC",
   fluid=TRUE,

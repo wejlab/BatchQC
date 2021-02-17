5a3f16b42883e1b2ac11965323794dabd9668037
rdconrad <73078126+rdconrad@users.noreply.github.com>
2021-02-17 15:56:41 -0500

    Editing formatting

--- inst/shiny/ui/ui_01_upload.R
+++ inst/shiny/ui/ui_01_upload.R
@@ -1,54 +1,60 @@
-
 accepted = c("text/csv",
              "text/comma-separated-values",
              "text/plain",
              ".csv")
 
 tabPanel("Upload Data",
-         useShinyjs(),
-         tags$style(appCSS),
-         tags$div(
-           class = "jumbotron",
-           tags$div(
-                 class = "container",
-                 fluidRow(
-                   column(7, h1("BatchQC"))
-                 ),
-                 tags$p("Batch Effects Quality Control Software"),
-                 uiOutput("tab")
+    useShinyjs(),
+    tags$style(appCSS),
+    tags$div(
+        class = "jumbotron",
+        tags$div(
+            class = "container",
+            fluidRow(column(7, h1("BatchQC"))),
+            tags$p("Batch Effects Quality Control Software"),
+            uiOutput("tab")
 
-           )
-         ),
+        )
+    ),
     # Application title
     titlePanel("Upload Data"),
 
     # Place for uploading data
     sidebarLayout(
         sidebarPanel(
-        h3("Upload counts and metadata table"),
-        tags$div(tags$p('Metadata file must contain "Sample" and "Batch" columns')),
-        fileInput("counts", "Counts table",
-                  multiple = FALSE,
-                  accept = accepted),
-        fileInput("md", "Metadata",
-                  multiple = FALSE,
-                  accept = accepted),
-        h3("Or upload a Summarized Experiment"),
-        fileInput("se", "Summarized Experiment",
-                  multiple = FALSE,
-                  accept = accepted)
+            h3("Upload counts and metadata table"),
+            tags$div(tags$p(
+                'Metadata file must contain "Sample" and "Batch" columns'
+            )),
+            fileInput(
+                "counts",
+                "Counts table",
+                multiple = FALSE,
+                accept = accepted
+            ),
+            fileInput("md", "Metadata",
+                      multiple = FALSE,
+                      accept = accepted),
+            h3("Or upload a Summarized Experiment"),
+            fileInput(
+                "se",
+                "Summarized Experiment",
+                multiple = FALSE,
+                accept = accepted
+            )
         ),
 
         # Show a table of the inputted data
         mainPanel(
             tabsetPanel(
-                tabPanel("Input",
+                tabPanel(
+                    "Input",
                     selectInput("covariate", "Select Covariate:", choices = ""),
                     tableOutput("summaryTable"),
                 ),
                 tabPanel("Confounding",
-                    textOutput("text")
-                    # tableOutput("confoundingTable")
+                         textOutput("text")
+                         # tableOutput("confoundingTable")))
                 )
             )
         )

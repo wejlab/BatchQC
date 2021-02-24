517d8683cb7feb18cc71ecf6f5dea00827401f08
rdconrad <73078126+rdconrad@users.noreply.github.com>
2021-02-24 18:23:30 -0500

    First commit of edits to make this into a working package

--- R/import.R
+++ R/import.R
@@ -1,10 +1,12 @@
-# Functions for importing data and summarizing experimental design
-library(SummarizedExperiment)
-library(dplyr)
-library(tidyr)
-
+#' This function allows you to create aa summarized experiment object
+#' @param counts_path path to counts file
+#' @param metadata_path path to metadata file
+#' @return a summarized experiment object
+#' @import SummarizedExperiment
+#'
+#' @export
 summarize_experiment = function(Counts_path,metadata_path) {
-  require(reader)
+  #require(reader)
   coldata=read.table(metadata_path,header = T,row.names = 1,check.names = F,sep = get.delim(metadata_path,n = 10,delims = c('\t',',')))
   counts=read.table(Counts_path,header = T,row.names = 1,check.names = F,sep = get.delim(Counts_path,n = 10,delims = c('\t',',')))
   counts=counts[rowSums(counts)>0,]
@@ -15,9 +17,20 @@ summarize_experiment = function(Counts_path,metadata_path) {
   se = SummarizedExperiment(assay=list(counts=counts
   ), colData=coldata)
 }
+
+
+#' This function allows you to ingest data
+#' @param se summarized experiment
+#' @param group biological covariate
+#' @param batch batch
+#' @import SummarizedExperiment
+#' @import EBSeq
+#' @return a summarized experiment object
+#'
+#' @export
 ingest_data <- function(se,group,batch){
-  require(SummarizedExperiment)
-  require(EBSeq)
+  #require(SummarizedExperiment)
+  #require(EBSeq)
 
   if (!is.null(se)) {
     variables=colnames(colData(se))
@@ -46,139 +59,6 @@ ingest_data <- function(se,group,batch){
   return(se)
 }
 
-batch_design <- function(se, covariate){
-  #' Create a batch design table for the provided covariate
-  design <- colData(se) %>% as_tibble %>% group_by(eval(as.symbol(covariate))) %>% count(Batch) %>% pivot_wider(names_from = Batch, values_from = n)
-  names(design)[names(design) == "eval(as.symbol(covariate))"] <- ""
-  for (i in 2:length(design)) {
-    colnames(design)[i] <- paste("Batch",i-1)
-  }
-  return(design)
-}
-
-cor_props <- function(bd){
-  #' Calculate correlation properties on a batch_design matrix `bd`
-
-  # Subset matrix to design only
-  m = bd[, -1, ]
-  rowsums = rowSums(m)
-  colsums = colSums(m)
-  tablesum = sum(rowsums)
-  expected = matrix(0, nrow(m), ncol(m))
-  for (i in 1:nrow(m)) {
-    for (j in 1:ncol(m)) {
-      expected[i, j] = rowsums[i] * colsums[j]/tablesum
-    }
-  }
-  chi = sum((m - expected)^2/expected)
-  mmin = min(nrow(m), ncol(m))
-
-  out = list("chi" = chi, "mmin"=mmin, "tablesum"=tablesum)
-  return(out)
-}
-
-std_pearson_corr_coef <- function(bd) {
-  #' Calculate standardized Pearson correlation coefficient
-  c <- cor_props(bd)
-  r <- sqrt(c$chi * c$mmin/((c$chi + c$tablesum) * (c$mmin - 1)))
-  return(r)
-}
-
-cramers_v <- function(bd) {
-  # Calculate Cramer's V
-  c <- cor_props(bd)
-  v <- sqrt(c$chi/(c$tablesum * (c$mmin - 1)))
-  return(v)
-}
-
-confound_metrics <- function(se){
-  covs = metadata(se)$covariates
-  metrics <- list("Pearson Correlation Coefficient"=std_pearson_corr_coef, "Cramer's V"=cramers_v)
-  metric.mat <- matrix(nrow=length(covs), ncol=length(metrics), dimnames = list(covs, names(metrics)))
-
-  for (c in covs){
-    # Get batch design
-    bd <- batch_design(se, c)
-    for (m in names(metrics)){
-      # Compute metric and place in appropriate slot
-      metric.mat[c, m] <- metrics[[m]](bd)
-    }
-  }
-  # Add metrics to se
-  return(metric.mat)
-}
-
-PCA_plotter <- function(se, assay, nfeature,color, shape ) {
-  data=se@assays@data[[assay]]
-  data=as.matrix(data)
-  data=apply(data,c(1,2),as.numeric)
-  data=data[rowSums(data)!=0,]
-  vargenes=apply(data,1,var)
-  vargenes=vargenes[order(vargenes,decreasing = T)]
-  vargenes=vargenes[seq(1,nfeature)]
-  data=log(data+1)
-  data=data[names(vargenes),]
-  data=data+1
-  for (i in 1:nrow(data)) {
-    data[i,]=(data[i,]-mean(data[i,]))/sd(data[i,])
-  }
-
-  coldata=data.frame(colData(se))
-  PCA=prcomp(t(data))
-  coldata=cbind(coldata,PCA$x)
-  coldata$sample=rownames(coldata)
-
-
-  plot=ggplot(coldata,aes_string(x='PC1',y='PC2',colour=color,shape=shape,sample = 'sample'))+geom_point(size=3)
-  return(list(PCA=PCA,plot=plot))
-}
 
 
 
-heatmap_plotter <- function(se, assay, nfeature,experiment_variable,annotation_column) {
-  data=se@assays@data[[assay]]
-  data=as.matrix(data)
-  data=apply(data,c(1,2),as.numeric)
-  data=data[rowSums(data)!=0,]
-  vargenes=apply(data,1,var)
-  vargenes=vargenes[order(vargenes,decreasing = T)]
-  vargenes=vargenes[seq(1,nfeature)]
-  data=log(data+1)
-  data=data[names(vargenes),]
-  data=data+1
-  for (i in 1:nrow(data)) {
-    data[i,]=(data[i,]-mean(data[i,]))/sd(data[i,])
-  }
-
-  coldata=data.frame(colData(se))
-
-  cor=cor(data)
-  if (!is.null(annotation_column)) {
-    if (length(annotation_column)==1) {
-      coldata=data.frame(coldata[,annotation_column],row.names = rownames(coldata))
-    }
-    else {
-      coldata=coldata[,annotation_column]
-    }
-    correlation_heatmap=pheatmap(cor,annotation_col = coldata,annotation_row = coldata,show_colnames = F,show_rownames = F
-                                 ,annotation_names_col = F,annotation_names_row = F,silent = T)
-
-    topn_heatmap = pheatmap(data,annotation_col = coldata,show_colnames = F,annotation_names_col = F,show_rownames = F,silent = T)
-
-    dendrogram=topn_heatmap$tree_col
-
-  }
-  else {
-    correlation_heatmap=pheatmap(cor,show_colnames = F,show_rownames = F
-                                 ,annotation_names_col = F,annotation_names_row = F,silent = T)
-
-    topn_heatmap = pheatmap(data,show_colnames = F,annotation_names_col = F,show_rownames = F,silent = T)
-
-    dendrogram=topn_heatmap$tree_col
-  }
-
-
-  return(list(correlation_heatmap=correlation_heatmap,
-              topn_heatmap=topn_heatmap,
-              dendrogram=dendrogram))
-}
--- inst/shiny/server/observer.R
+++ inst/shiny/server/observer.R
@@ -4,7 +4,7 @@ observeEvent( input$counts, {
   #reactivevalue$counts = input$counts
   reactivevalue$counts_location=input$counts$datapath
   output$counts_header=renderTable((read.table(reactivevalue$counts_location,header = T,row.names = 1,
-                                                   sep = get.delim(reactivevalue$counts_location,n = 10,delims = c('\t',',')),check.names = F))[seq(1,10),seq(1,3)])
+                                               sep = get.delim(reactivevalue$counts_location,n = 10,delims = c('\t',',')),check.names = F))[seq(1,10),seq(1,3)])
 })
 
 #### Obtain the metadata matrix and metadata table location ####
@@ -13,11 +13,9 @@ observeEvent( input$md, {
   if (is.null(input$md)) return()
   reactivevalue$metadata_location=input$md$datapath
   output$metadata_header=renderTable(head(read.table(reactivevalue$metadata_location,header = T,row.names = 1,
-                                                   sep = get.delim(reactivevalue$metadata_location,n = 10,delims = c('\t',',')),check.names = F)))
+                                                     sep = get.delim(reactivevalue$metadata_location,n = 10,delims = c('\t',',')),check.names = F)))
 
   reactivevalue$metadata = read.table(reactivevalue$metadata_location,header = T,row.names = 1,sep = get.delim(reactivevalue$metadata_location,n = 10,delims = c('\t',',')))
-
-
 })
 
 #### Obtain Count matrix and metadata from the rds summarizeexperiment ####
@@ -27,8 +25,6 @@ observeEvent( input$se, {
 
   se=readRDS(input$se$datapath)
   reactivevalue$se=se
-
-
 })
 
 observeEvent( input$submit, {
@@ -97,7 +93,7 @@ observeEvent(input$submit_variables, {
     reactivevalue$batch_Variable_Name = input$batch
     reactivevalue$group_variable_Name = input$group
     variable_overview=data.frame(table(reactivevalue$metadata[,reactivevalue$group_variable_Name],
-                                              reactivevalue$metadata[,reactivevalue$batch_Variable_Name]))
+                                       reactivevalue$metadata[,reactivevalue$batch_Variable_Name]))
     colnames(variable_overview)=c('Biological Group','Batch Group','Number of Sample')
     output$variable_overview=renderDataTable({variable_overview})
   }
@@ -111,17 +107,17 @@ observeEvent(input$submit_variables, {
 observeEvent(input$DESEQ_normalization, {
   if (is.null(input$DESEQ_normalization)) return()
   if (!is.null(reactivevalue$se)){require(EBSeq)
-  reactivevalue$se@assays@data$DESEQ_normalization=GetNormalizedMat(reactivevalue$se@assays@data$counts, MedianNorm(reactivevalue$se@assays@data$counts))
-  updateSelectizeInput(session = session,inputId = 'Normalization_method_heatmap',choices = assayNames((reactivevalue$se)),selected = NULL)
-  updateSelectizeInput(session = session,inputId = 'Normalization_method_PCA',choices = assayNames((reactivevalue$se)),selected = NULL)}
+    reactivevalue$se@assays@data$DESEQ_normalization=GetNormalizedMat(reactivevalue$se@assays@data$counts, MedianNorm(reactivevalue$se@assays@data$counts))
+    updateSelectizeInput(session = session,inputId = 'Normalization_method_heatmap',choices = assayNames((reactivevalue$se)),selected = NULL)
+    updateSelectizeInput(session = session,inputId = 'Normalization_method_PCA',choices = assayNames((reactivevalue$se)),selected = NULL)}
 })
 
 observeEvent(input$CPM_Normalization, {
   if (is.null(input$CPM_Normalization)) return()
-if (!is.null(reactivevalue$se)){  reactivevalue$se@assays@data$CPM=((reactivevalue$se@assays@data$counts+1)/colSums(reactivevalue$se@assays@data$counts))*(10^6)
+  if (!is.null(reactivevalue$se)){  reactivevalue$se@assays@data$CPM=((reactivevalue$se@assays@data$counts+1)/colSums(reactivevalue$se@assays@data$counts))*(10^6)
   updateSelectizeInput(session = session,inputId = 'Normalization_method_heatmap',choices = assayNames((reactivevalue$se)),selected = NULL)
   updateSelectizeInput(session = session,inputId = 'Normalization_method_PCA',choices = assayNames((reactivevalue$se)),selected = NULL)}
-  })
+})
 
 #### Organize the variables, ready the variable names for latter analysis ####
 observe({
@@ -133,47 +129,40 @@ observe({
       )
       updateSelectizeInput(session,'group',choices=colnames(reactivevalue$metadata),selected = NULL
       )
-}
+    }
     else {
       reactivevalue$covariates=colnames(reactivevalue$metadata)[!colnames(reactivevalue$metadata)%in%
                                                                   c(reactivevalue$group_variable_Name,reactivevalue$batch_Variable_Name)]
       updateSelectInput(session = session,inputId = "covariate", choices = reactivevalue$covariates)
-      }
+    }
   }
 })
 
 
 
 
-
-
-
-
-
-
 #### Plot Heatmap based on the input ####
 observeEvent( input$heatmap_plot, {
-if (!is.null(reactivevalue$se)) {
+  if (!is.null(reactivevalue$se)) {
 
-results=heatmap_plotter(reactivevalue$se,
-                        input$Normalization_method_heatmap,
-                        input$top_n_heatmap,
-                        reactivevalue$group_variable_Name,
-                        input$Variates_to_display)
+    results=heatmap_plotter(reactivevalue$se,
+                            input$Normalization_method_heatmap,
+                            input$top_n_heatmap,
+                            reactivevalue$group_variable_Name,
+                            input$Variates_to_display)
 
-output$correlation_heatmap=renderPlot({
-  results$correlation_heatmap
-})
-output$topn_heatmap=renderPlot({
-  results$topn_heatmap
-})
+    output$correlation_heatmap=renderPlot({
+      results$correlation_heatmap
+    })
+    output$topn_heatmap=renderPlot({
+      results$topn_heatmap
+    })
 
-output$Dendrogram=renderPlot({
-  plot(results$dendrogram)
+    output$Dendrogram=renderPlot({
+      plot(results$dendrogram)
+    })
+  }
 })
-}
-}
-)
 
 
 
@@ -186,10 +175,10 @@ observeEvent( input$PCA_plot, {
     require(ggplot2)
     require(plotly)
     results=PCA_plotter(reactivevalue$se,
-                    input$Normalization_method_PCA,
-                    input$top_n_PCA,
-                    input$Variates_color,
-                    shape=input$Variates_shape)
+                        input$Normalization_method_PCA,
+                        input$top_n_PCA,
+                        input$Variates_color,
+                        shape=input$Variates_shape)
     output$PCA=renderPlot({(results[['plot']])})
 
   }

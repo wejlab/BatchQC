5bf332a60c13f073266de0cae8892479178a2f6b
Michael Silverstein <msilverstein@finchtherapeutics.com>
2021-03-05 11:23:51 -0500

    redesign UI

--- inst/shiny/ui/ui_02_experimental_design.R
+++ inst/shiny/ui/ui_02_experimental_design.R
@@ -1,19 +1,26 @@
 
-tabPanel("Experimental Design",
 
-         # Application title
-         titlePanel("Experimental Design"),
 
-         # Sidebar with a slider input for number of bins
-         tabsetPanel(
-           tabPanel(
-             "Input",
-             selectInput("covariate", "Select Covariate:", choices = ""),
-           ),
-           tabPanel("Confounding",
-                    textOutput("text"),
-                    tableOutput("confoundingTable")
-           )
-         )
+tabPanel(
+  "Experimental Design",
 
+  # Application title
+  titlePanel("Experimental Design"),
+
+  sidebarLayout(sidebarPanel(
+    h3("Experimental Design"),
+    selectInput('design_batch', 'Select Batch Variable', choices = "")
+  ),
+  mainPanel(tabsetPanel(
+    tabPanel(
+      "Batch Design",
+
+      selectInput("design_covariate", "Select Covariate:", choices = ""),
+    ),
+    tabPanel(
+      "Confounding Statistics",
+      textOutput("text"),
+      tableOutput("confoundingTable")
+    )
+  )))
 )

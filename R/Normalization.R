95ba4efbe7bbad0d79d857d555d7a6a80dee749c
Jessica <102758461+jessmcc22@users.noreply.github.com>
2022-06-09 10:34:06 -0400

    Revert "Improved dendrogram and circular dendrogram vizs."

--- R/Normalization.R
+++ R/Normalization.R
@@ -10,18 +10,18 @@
 #'
 #' @export
 normalize_SE <- function(se, method, assay_to_normalize, output_assay_name) {
-  se <- se
-  if (method == 'CPM') {
-    se@assays@data[[output_assay_name]] <-
-      (se@assays@data[[assay_to_normalize]] +1) /
-      colSums(se@assays@data[[assay_to_normalize]])*(10^6)
-    
-  }
-  else if (method == 'DESeq') {
-    se@assays@data[[output_assay_name]] <- GetNormalizedMat(
-      se@assays@data[[assay_to_normalize]],
-      MedianNorm(se@assays@data[[assay_to_normalize]]))
-    
-  }
-  return(se)
+    se <- se
+    if (method == 'CPM') {
+        se@assays@data[[output_assay_name]] <-
+            (se@assays@data[[assay_to_normalize]] +1) /
+            colSums(se@assays@data[[assay_to_normalize]])*(10^6)
+
+    }
+    else if (method == 'DESeq') {
+        se@assays@data[[output_assay_name]] <- GetNormalizedMat(
+            se@assays@data[[assay_to_normalize]],
+            MedianNorm(se@assays@data[[assay_to_normalize]]))
+
+    }
+    return(se)
 }

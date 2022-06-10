8d2e3e9ff182cc2a488cf86a86735648896e50ff
Evan Holmes <evanholmes@Evans-MacBook-Pro-3.local>
2022-06-10 17:57:23 -0400

    Implemented DE analysis and analysis results visualization

--- R/DifferentialExpression.R
+++ R/DifferentialExpression.R
@@ -1,33 +1,85 @@
 globalVariables(c("chosen"))
 
-#' This function allows you to Add normalized count matrix to the SE object
-#' @param se SummarizedExperiment
-#' @param method Differential Expression Analysis Method
-#' @param conditions Condition covariate of interest
-#' @param assay_to_analyze Which assay use to do DE analysis
-#' @param output_assay_name name of results assay
-#' @return a summarized experiment object with normalized assay appended.
+#' This function allows you to plot DE analysis results as a volcano plot
+#' @param volcano_data a dataframe of expression change and p-value data
+#' @param slider magnitude of significance value threshold
+#' @return a volcano plot of expression change and p-value data
+#' @import ggplot2
+#' @export
+volcano_plot = function(volcano_data,slider) {
+    
+    slider_cond <- NULL
+    volcano_data <- as.data.frame(volcano_data)
+    slider_factor <- (1 * (10^slider))
+    volcano_data <- volcano_data %>%
+        mutate(slider_cond <- case_when(volcano_data[, 2] < 
+                                            slider_factor ~ "TRUE", 
+                                       volcano_data[, 2] >= 
+                                           slider_factor ~ "FALSE",
+                                       TRUE ~ 'NA'))
+    
+    p <- ggplot(data=volcano_data, aes(x=volcano_data[,1],
+                                       y=-log10(volcano_data[,2]),
+                                       color=slider_cond)) + 
+        geom_point() +
+        scale_color_manual(values = c('FALSE' = 'red', 'TRUE' = 'orange',
+                                      'NA'='black')) + 
+        xlab("Change in Expression") +
+        ylab("Signifigance Value") +
+        theme(legend.position="bottom")
+    
+    return(p)
+}
+
+#' This function runs DE analysis on a count matrix in the se object
+#' @param se summarizedExperiment
+#' @param method DE analysis method
+#' @param conditions covariate for DE analysis
+#' @param assay_to_analyze assay for DE analysis
+#' @return DE analysis results
 #' @import SummarizedExperiment
-#' @import reader
-#' @import EBSeq
+#' @import scran
+#' @import DESeq2
 #'
 #' @export
-analyze_SE <- function(se, method, conditions, assay_to_analyze,
-                        output_assay_name) {
-    se <- se
-    if (method=='wilcox') {
-        output_assay_name <- scran::findMarkers(se, se@colData@data$conditions,
-                                                test.type=method,
-                                                pval.type="all",
-                                                lfc=1,
-                                                subset.row=chosen)
-
+analyze_SE <- function(se, method, conditions, assay_to_analyze) {
+    data <- se@assays@data[[assay_to_analyze]]
+    rownames(data) <- names(se)
+    analysis_design <- as.data.frame(se@colData[conditions])
+    con <- paste("~ ",colnames(analysis_design))
+    
+    if (method=='t') {
+        res <- findMarkers(data, analysis_design[,1],
+                           test.type=method, pval.type="all", lfc=1)
+        res <- as.matrix(res[[1]])
+        pvalue <- res[,2]
+        log2FC <- res[,4]
+        res <- res[order(res[,1],decreasing=FALSE),]
+        to_plot <- cbind(log2FC,pvalue)
     }
-    else if (method=='DESeq') {
-        se@assays@data[[output_assay_name]] <- GetNormalizedMat(
-            se@assays@data[[assay_to_analyze]],
-            MedianNorm(se@assays@data[[assay_to_analyze]]))
-
+    else if (method=='wilcox') {
+        res <- findMarkers(data, analysis_design[,1],
+                           test.type=method, pval.type="all", lfc=1)
+        res <- as.matrix(res[[1]])
+        pvalue <- res[,2]
+        AUC <- res[,3]
+        res <- res[order(res[,1],decreasing=FALSE),]
+        to_plot <- cbind(AUC,pvalue)
     }
-    return(se)
+    else if (method=='DESeq2') {
+        colnames(data) <- rownames(analysis_design)
+        data[is.na(data)] <- 0
+        dds <- DESeqDataSetFromMatrix(countData = abs(round(data)),
+                                      colData = analysis_design,
+                                      design = stats::as.formula(con))
+        res <- DESeq(dds)
+        res <- results(res)
+        pvalue <- res$pvalue
+        log2FC <- res$log2FoldChange
+        res <- as.matrix(res)
+        res <- res[order(res[,6],decreasing=FALSE),]
+        to_plot <- cbind(log2FC,pvalue)
+    }
+    return(list(res=res,
+                volcano=to_plot))
 }
--- inst/shiny/server/server_06_differentialExpression.R
+++ inst/shiny/server/server_06_differentialExpression.R
@@ -1,1 +1,16 @@
 ### DIFFERENTIAL EXPRESSION ANALYSIS TAB ###
+observeEvent(input$DE_analyze, {
+    req(reactivevalue$se, input$DE_analyze)
+    
+    results <- analyze_SE(reactivevalue$se, input$DE_method, 
+                          input$DE_conditions, input$DE_assay)
+    output$DE_results <- renderDT({results$res
+    })
+    output$volcano <- renderPlot({
+        volcano_plot(results$volcano,input$slider)
+    }, height = function() {session$clientData$output_volcano_width
+    })
+    
+})
+
+

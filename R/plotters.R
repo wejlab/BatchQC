fa3849fba61f957232035a61b73feed186e7139d
jessmcc22Mac <jessmcc@bu.edu>
2022-06-23 10:27:18 -0400

    Fixed PCA graphs so that all metadata columns are discrete variables and can be graphed as shape OR color; created function to plot the pca graph and shorten the PCA function; updated description of value returned from PCA_plotter

--- R/plotters.R
+++ R/plotters.R
@@ -91,9 +91,11 @@ preprocess <- function(se, assay, nfeature){
     data <- as.matrix(data)
     data <- apply(data, c(1, 2), as.numeric)
     data <- data[rowSums(data) != 0, ]
+
     vargenes <- apply(data, 1, stats::var)
     vargenes <- vargenes[order(vargenes, decreasing = TRUE)]
     vargenes <- vargenes[seq(1, nfeature)]
+
     data <- log(data + 1)
     data <- data[names(vargenes), ]
     data <- data + 1
@@ -109,8 +111,7 @@ preprocess <- function(se, assay, nfeature){
 #' @param assays array of assay names from `se`
 #' @param xaxisPC the PC to plot as the x axis
 #' @param yaxisPC the PC to plot as the y axis
-#' @import ggplot2
-#' @return PCA plot
+#' @return List containing PCA info, PCA variance and PCA plot
 #'
 #' @export
 PCA_plotter <- function(se, nfeature, color, shape, assays, xaxisPC, yaxisPC) {
@@ -127,43 +128,70 @@ PCA_plotter <- function(se, nfeature, color, shape, assays, xaxisPC, yaxisPC) {
             centered <- data - rowMeans(data)/matrixStats::rowSds(data)
             coldata <- data.frame(colData(se))
             pca <- stats::prcomp(t(centered), center = FALSE)
+
             # Get variance explained
             var_explained <- summary(pca)$importance["Proportion of Variance",]
             var_explained_df <- stats::setNames(as.data.frame(var_explained),
                                                 assay)
             if (is.null(var_explained_data)){
                 var_explained_data <- var_explained_df
-            }
-            else{
+            }else{
                 var_explained_data <- cbind(var_explained_data,
                                             var_explained_df)
             }
+
             # Extract PC data
             pca_data <- as.data.frame(pca$x)
+
             # Annotate with assay name
             pca_data['assay'] <- assay
+
             # Merge metadata
             pca_md <- cbind(coldata, pca_data)
             pca_md$sample <- rownames(coldata)
+
             # Add to data
             pca_plot_data <- rbind(pca_plot_data, pca_md)
         }
     }
     # Reorder data
     pca_plot_data$assay <- factor(pca_plot_data$assay, levels = assays)
+
     var_explained_data <- var_explained_data[c(xaxisPC, yaxisPC), , drop=FALSE]
-    xaxisPC <- paste0('PC', xaxisPC)
-    yaxisPC <- paste0('PC', yaxisPC)
-    plot <- ggplot(pca_plot_data,
-                    aes_string(x = xaxisPC, y = yaxisPC, color = color,
-                                shape = shape, sample = 'sample')) +
-            geom_point(size = 3) +
-            facet_wrap(vars(assay), ncol = 2, scales = 'free')
+
+    plot <- plot_data(pca_plot_data, color, shape, xaxisPC, yaxisPC)
+
     return(list(PCA = pca_plot_data,
                 var_explained = var_explained_data,
                 plot = plot))
 }
 
+#' This function formats the PCA plot using ggplot
+#' @param pca_plot_data Data for all assays to plot
+#' @param color variable that will be plotted as color
+#' @param shape variable that will be plotted as shape
+#' @param xaxisPC the PC to plot as the x axis
+#' @param yaxisPC the PC to plot as the y axis
+#' @import ggplot2
+#' @return PCA plot
+
+plot_data <- function(pca_plot_data, color, shape, xaxisPC, yaxisPC){
+    pca_plot_data[, c(color)] <- factor(pca_plot_data[, color])
+    pca_plot_data[, c(shape)] <- factor(pca_plot_data[, shape])
+
+    xaxisPC <- paste0('PC', xaxisPC)
+    yaxisPC <- paste0('PC', yaxisPC)
+    PCAplot <- ggplot(pca_plot_data,
+                      aes_string(x = xaxisPC,
+                                y = yaxisPC,
+                                color = color,
+                                shape = shape,
+                                sample = 'sample')) +
+                geom_point(size = 3) +
+                facet_wrap(vars(assay), ncol = 2, scales = 'free')
+
+    return(PCAplot)
+}
 
 #' This function allows you to plot a heatmap
 #' @param se SummarizedExperiment

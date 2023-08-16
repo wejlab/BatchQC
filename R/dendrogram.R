e8ee3cc644be64725693dd3dcade972a64c84d03
jessmcc22Mac <jessmcc@bu.edu>
2023-08-16 15:48:26 -0400

    Added in variable for labels for dendogram; updated Intro command line instructions; corrected shiny app label

--- R/dendrogram.R
+++ R/dendrogram.R
@@ -4,7 +4,7 @@
 #' This function processes count data for dendrogram plotting
 #' @param se SummarizedExperiment object
 #' @param assay assay to plot
-#' @param annotation_column sample metadata column
+#' @param batch_var sample metadata column
 #' @import tibble
 #' @import ggdendro
 #' @import dplyr
@@ -14,7 +14,7 @@
 #' @example R/examples/process_dendrogram.R
 #'
 #' @export
-process_dendrogram <- function(se, assay, annotation_column) {
+process_dendrogram <- function(se, assay, batch_var) {
 
     data <- t(se@assays@data[[assay]])
     dat <- as.data.frame(data) %>%
@@ -49,7 +49,8 @@ process_dendrogram <- function(se, assay, annotation_column) {
 #' This function creates a dendrogram plot
 #' @param se SummarizedExperiment object
 #' @param assay assay to plot
-#' @param annotation_column sample metadata column
+#' @param batch_var sample metadata column representing batch
+#' @param category_var sample metadata column representing category of interest
 #' @import ggdendro
 #' @import RColorBrewer
 #' @import dplyr
@@ -59,20 +60,24 @@ process_dendrogram <- function(se, assay, annotation_column) {
 #' @example R/examples/dendrogram_plotter.R
 #'
 #' @export
-dendrogram_plotter <- function(se, assay, annotation_column) {
+dendrogram_plotter <- function(se, assay, batch_var, category_var) {
+    # Need to add in an if statement
+    # if(batch_var == "batch_var"){
+    # rename the "batch_var" column to "batch"
+    #}
 
-    dends <- process_dendrogram(se, assay, annotation_column)
+    dends <- process_dendrogram(se, assay, batch_var)
 
     dendrogram_ends <- dends$dendrogram_ends
 
     dendrogram_segments <- dends$dendrogram_segments
 
-    unique_vars <- levels(factor(dendrogram_ends[,annotation_column])) %>%
+    unique_vars <- levels(factor(dendrogram_ends[,batch_var])) %>%
         as.data.frame() %>% rownames_to_column("row_id")
 
     color_count <- length(unique(unique_vars$.))
     get_palette <- grDevices::colorRampPalette(brewer.pal(
-        n = length(unique(dendrogram_ends[,annotation_column])),
+        n = length(unique(dendrogram_ends[,batch_var])),
         name = "Paired"))
     palette <- get_palette(color_count) %>% as.data.frame() %>%
         rename("color" = ".") %>%
@@ -87,10 +92,10 @@ dendrogram_plotter <- function(se, assay, annotation_column) {
             aes(x=x, y=y, xend=xend, yend=yend)) +
         geom_segment(data = dendrogram_ends,
             aes(x=x, y=y.x, xend=xend, yend=yend,
-                color = dendrogram_ends[,annotation_column])) +
+                color = dendrogram_ends[,batch_var])) +
         scale_color_manual(values = annotation_color,
             limits = names(annotation_color),
-            name = as.character(annotation_column)) +
+            name = as.character(batch_var)) +
         scale_y_reverse() +
         coord_flip() +
         theme(axis.text.y=element_blank(), axis.ticks.y=element_blank()) +
--- inst/shiny/ui/ui_06_dendrogram.R
+++ inst/shiny/ui/ui_06_dendrogram.R
@@ -1,17 +1,17 @@
 tabPanel("Dendrograms",
-         
+
          # Application title
          titlePanel("Dendrograms"),
-         
+
          sidebarLayout(
              sidebarPanel(
                  selectizeInput('dend_assay_name','Choose assay to display',choices =c(),multiple = FALSE,selected = NULL,
                                 options=list(placeholder = 'Please select an option below',onInitialize = I('function() { this.setValue(""); }'))),
-                 selectizeInput('dend_variates_to_display','Choose variate(s) to display on heatmap',
+                 selectizeInput('dend_variates_to_display','Choose variate(s) to display on dendogram',
                                 choices =c(),multiple = TRUE,selected = NULL),
                  actionButton('dend_plot',label = 'Here we go!')
              ),
-             
+
              # Show a plot of the dendrograms
              mainPanel(
                  tabsetPanel(

f34af64bdbef181bd103b3543ac1c411dd0665df
Solomon <sj1234520162016@gmail.com>
2023-09-01 08:17:25 -0400

    Added the updated dendrogram server file

--- inst/shiny/server/server_05_dendrogram.R
+++ inst/shiny/server/server_05_dendrogram.R
@@ -8,16 +8,14 @@ observeEvent(input$dend_plot, {
                                 input$dend_assay_name,
                                 input$dend_batch_to_display,
                                 input$dend_category_to_display)$dendrogram)
-    }, height = function() {
-        session$clientData$output_dendrogram_width
+    }, height = function() {session$clientData$output_dendrogram_width
     })
 
     output$circular_dendrogram <- renderPlot({
         plot(dendrogram_plotter(reactivevalue$se,
                                 input$dend_assay_name,
                                 input$dend_batch_to_display,
-                            input$dend_category_to_display)$circular_dendrogram)
-    }, height = function() {
-        session$clientData$output_circular_dendrogram_width
+                                input$dend_category_to_display)$circular_dendrogram)
+    }, height = function() {session$clientData$output_circular_dendrogram_width
     })
 })

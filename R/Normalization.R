59cc1a805fedc25286ddde39d0d5153aaf055d5f
Solomon <sj1234520162016@gmail.com>
2023-09-11 00:39:06 -0400

    Updated the example for normalize_SE within the documentation rather than a separate file

--- R/Normalization.R
+++ R/Normalization.R
@@ -7,7 +7,19 @@
 #' @import SummarizedExperiment
 #' @import reader
 #' @import EBSeq
-#' @example R/examples/normalize_SE.R
+#' @examples
+#' library(scran)
+#' se <- mockSCE()
+#' se_CPM_normalized <- BatchQC::normalize_SE(se, method = "CPM",
+#'                                  assay_to_normalize = "counts",
+#'                                  output_assay_name =
+#'                                      "CPM_normalized_counts")
+#' se_DESeq_normalized <- BatchQC::normalize_SE(se, method = "DESeq",
+#'                                  assay_to_normalize = "counts",
+#'                                  output_assay_name =
+#'                                      "DESeq_normalized_counts")
+#' se_CPM_normalized
+#' se_DESeq_normalized
 #'
 #' @export
 normalize_SE <- function(se, method, assay_to_normalize, output_assay_name) {

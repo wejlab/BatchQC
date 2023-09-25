122863bf05c01578a6aa345308082cf8babf659e
Solomon <sj1234520162016@gmail.com>
2023-09-25 15:40:10 -0400

    Updated the code with new color palette integration

--- R/dendrogram_color_palette.R
+++ R/dendrogram_color_palette.R
@@ -23,24 +23,15 @@
 dendrogram_color_palette <- function(col, dendrogram_info) {
 
     # Create unique_vars dataframe
-    unique_vars <- levels(factor(dendrogram_info[, col])) %>%
+    unique_vars <- unique(dendrogram_info[, col]) %>%
         as.data.frame() %>% rownames_to_column("row_id")
 
     # Determine color count and palette
-    color_count <- length(unique(unique_vars$.))
-    n <- length(unique(dendrogram_info[, col]))
-    if (n < 5) {
-        get_palette <- function(n) {
-            hues <- seq(25, 375, length = n + 1)
-            grDevices::hcl(h = hues, l = c(40, 65), c = 100)[seq_len(n)]
-        }
-    } else {
-        get_palette <- function(n) {
-            hues <- seq(376, 1500, length = n + 1)
-            grDevices::hcl(h = hues, l = c(35, 65), c = 100)[seq_len(n)]
-        }
-    }
-    palette <- get_palette(color_count) %>%
+    color_count <- length(unique_vars$.)
+
+    color_list <- color_palette(n = color_count, first_hue = 50)
+
+    palette <- color_list %>%
         as.data.frame() %>%
         dplyr::rename("color" = ".") %>%
         rownames_to_column(var = "row_id")

f7f60c4c976be36e1686b020607971aabefa09de
Solomon <sj1234520162016@gmail.com>
2023-10-03 14:52:43 -0400

    Updated Heatmap ui and server to fix the merge request by  adding start and end value for features.

--- inst/shiny/server/server_04_heatmap.R
+++ inst/shiny/server/server_04_heatmap.R
@@ -3,28 +3,45 @@
 ## Plot heatmap
 observeEvent(input$heatmap_plot, {
     req(reactivevalue$se)
-    validate(need(input$top_n_heatmap <= dim(reactivevalue$se)[1] &&
-                      input$top_n_heatmap > 1,
+    validate(need(input$end_n_heatmap <= dim(reactivevalue$se)[1] &&
+                      input$end_n_heatmap > 1,
                   "Please select between 2 and the size of your data set
-                variates to display"))
+                variables to display"))
     results <- heatmap_plotter(reactivevalue$se,
                             input$heatmap_assay_name,
-                            input$top_n_heatmap,
+                            input$end_n_heatmap,
                             input$variates_to_display,
                             input$log_option)
 
     output$correlation_heatmap <- renderPlot({
-        validate(need(input$top_n_heatmap <= dim(reactivevalue$se)[1] &&
-                          input$top_n_heatmap > 1,
-                      "Please select between 2 and the size of your data set variates to display"))
+        validate(
+            need(input$start_n_heatmap >= 1 &&
+                     input$end_n_heatmap <= dim(reactivevalue$se)[1] &&
+                     input$start_n_heatmap < input$end_n_heatmap,
+                 "Please enter start value which is less than end value"),
+            need(input$end_n_heatmap <= dim(reactivevalue$se)[1] &&
+                     input$end_n_heatmap > input$start_n_heatmap,
+                 paste0("Please enter end value between ",
+                        input$start_n_heatmap + 1, " and ",
+                        dim(reactivevalue$se)[1]))
+        )
         results$correlation_heatmap
         }, height = function() {
         session$clientData$output_correlation_heatmap_width
     })
 
     output$topn_heatmap <- renderPlot({
-        validate(need(input$top_n_heatmap <= dim(reactivevalue$se)[1],
-                      "Value must be less than the dim of your data set"))
+        validate(
+            need(input$start_n_heatmap >= 1 &&
+                     input$end_n_heatmap <= dim(reactivevalue$se)[1] &&
+                     input$start_n_heatmap < input$end_n_heatmap,
+                 "Please enter start value which is less than end value"),
+            need(input$end_n_heatmap <= dim(reactivevalue$se)[1] &&
+                     input$end_n_heatmap > input$start_n_heatmap,
+                 paste0("Please enter end value between ",
+                        input$start_n_heatmap + 1, " and ",
+                        dim(reactivevalue$se)[1]))
+        )
         results$topn_heatmap
     }, height = function() {
         session$clientData$output_topn_heatmap_width

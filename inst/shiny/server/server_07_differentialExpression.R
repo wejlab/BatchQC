374f1d77e3c9b1612174f9232e44afaa7363e247
jessmcc22Mac <jessmcc@bu.edu>
2024-01-30 13:56:16 -0500

    Cleaned up DiffExpression file of unneeded code

--- inst/shiny/server/server_07_differentialExpression.R
+++ inst/shiny/server/server_07_differentialExpression.R
@@ -17,43 +17,18 @@ observeEvent(input$DE_analyze, {
             input$DE_assay)
         setProgress(1, 'Complete!')
     })
-### figure this stuff out
-    display_covariate <- names(reactivevalue$DE_results)[length(reactivevalue$DE_results)] #the last item in resultsNames list
-    #pval_summary_table <- pval_summary(reactivevalue$DE_results)
-    # volcano <- abind(DESeq2::results(reactivevalue$DE_results$dds,
-    #     name = display_covariate)$log2FoldChange,
-    #     DESeq2::results(reactivevalue$DE_results$dds,
-    #         name = display_covariate)$pvalue,
-    #     rownames(DESeq2::results(reactivevalue$DE_results$dds,
-    #         name = display_covariate)), along = 2)
-    # volcano <- as.data.frame(volcano)
-    # volcano[, 1:2] <- sapply(volcano[, 1:2], as.numeric)
-    # results_tab <- as.data.frame(DESeq2::results(reactivevalue$DE_results$dds,
-    #     name = display_covariate))
+
+    display_covariate <- names(reactivevalue$DE_results)[length(reactivevalue$DE_results)]
 
     output$DE_results <- renderDT({
         reactivevalue$DE_results[[length(reactivevalue$DE_results)]]
     }) #this only displays the results for the last analysis; need to update to include all analysis
 
-    output$pval_summary <- renderDT({pval_summary(reactivevalue$DE_results)}) #,
-    #     rownames = TRUE,
-    #     striped = TRUE,
-    #     bordered = TRUE,
-    #     caption = "<b> <span style='color:#000000'> P-Value Summary Table </b>",
-    #     caption.placement = getOption("xtable.caption.placement", "top"),
-    # )
+    output$pval_summary <- renderDT({pval_summary(reactivevalue$DE_results)})
 
     output$covariate_pval_plot <- renderPlot({
         covariate_pval_plotter(reactivevalue$DE_results)
     })
-
-    #output$batch_pval_plot <- renderPlot({
-    #    batch_pval_plotter(reactivevalue$DE_results$dds)
-    #})
-
-    # output$volcano <- renderPlotly({
-    #     volcano_plot(reactivevalue$DE_results[[length(names(reactivevalue$DE_results))]], input$pslider, input$fcslider)
-    # })
 
     output$downloadDEData <- downloadHandler(
         filename = function() {
@@ -114,36 +89,3 @@ observeEvent(input$result_to_view, {
         }
     )
 })
-# observeEvent(input$DE_res_selected, {
-#     req(reactivevalue$DE_results)
-#
-#     display_covariate <- as.character(input$DE_res_selected)
-#
-#     # volcano <- abind(DESeq2::results(reactivevalue$DE_results$dds,
-#     #     name = display_covariate)$log2FoldChange,
-#     #     DESeq2::results(reactivevalue$DE_results$dds,
-#     #         name = display_covariate)$pvalue,
-#     #     rownames(DESeq2::results(reactivevalue$DE_results$dds,
-#     #         name = display_covariate)), along = 2)
-#     # volcano <- as.data.frame(volcano)
-#     # volcano[, 1:2] <- sapply(volcano[, 1:2], as.numeric)
-#     # results_tab <- as.data.frame(DESeq2::results(reactivevalue$DE_results$dds,
-#     #     name = display_covariate))
-#
-#     output$DE_results <- renderDT({
-#         reactivevalue$DE_results[[display_covariate]]
-#     })
-#
-#     output$volcano <- renderPlotly({
-#         volcano_plot(reactivevalue$DE_results[[display_covariate]], input$pslider, input$fcslider)
-#     })
-#
-#     output$downloadDEData <- downloadHandler(
-#         filename = function() {
-#             paste("DE_results", ".csv", sep = "")
-#         },
-#         content = function(file) {
-#             write.csv(reactivevalue$DE_results[[display_covariate]], file)
-#         }
-#     )
-# })

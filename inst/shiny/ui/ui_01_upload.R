b66c90da4fc656436f497be6b2ed0064b49659bc
jessmcc22Mac <jessmcc@bu.edu>
2024-03-29 12:04:55 -0400

    Added descriptions and references for each example data

--- inst/shiny/ui/ui_01_upload.R
+++ inst/shiny/ui/ui_01_upload.R
@@ -55,7 +55,7 @@ tabPanel("Upload Data",
                              selectInput("exampleData",
                                  "Example Data",
                                  choices = c("", "proteinData", "signatureData",
-                                     "bladderData", "No Selection"),
+                                     "bladderData", "TbData", "No Selection"),
                                  selected = ""), ),
             withBusyIndicatorUI(actionButton(inputId = 'submit',
                                             label = 'Upload'))
@@ -65,15 +65,33 @@ tabPanel("Upload Data",
         mainPanel(
             tabsetPanel(
                 tabPanel('Input Summary',
-                         h4(strong("Usage")),
-                         h5("The table displayed here is a preview of the data selected to be uploaded to BatchQC. Please take a moment to verify that this is the intended data to upload, then select the Upload button to complete uploading the data to BatchQC."),
-                         h4(strong("Data Preview")),
-                         DTOutput('counts_header'),
-                         textOutput('counts_dimensions'),
-                         DTOutput('se_counts'),
-                         textOutput('se_dimensions'),
-                         br()
-                         ),
+                        h4(strong("Usage")),
+                        h5("The table displayed here is a preview of the data selected to be uploaded to BatchQC. Please take a moment to verify that this is the intended data to upload, then select the Upload button to complete uploading the data to BatchQC."),
+                        conditionalPanel(condition = "input.uploadChoice == 'example'",
+                            h4(strong("Example Data Description")),
+                            conditionalPanel(condition = "input.exampleData == 'proteinData'",
+                                h5("This data set is mass-spectromery data for protein expression in an unpublished data set.")
+                                ),
+                            conditionalPanel(condition = "input.exampleData == 'signatureData'",
+                                h5("This data set is RNA-seq data from signature data captured when activating different growth pathway genes in human mammary epithelial cells (GEO accession: GSE73628)."),
+                                h6("Reference: Rahman M, et al. Activity of distinct growth factor receptor network components in breast tumors uncovers two biologically relevant subtypes. Genome Med. 2017 Apr 26;9(1):40. doi: 10.1186/s13073-017-0429-x.")
+                            ),
+                            conditionalPanel(condition = "input.exampleData == 'bladderData'",
+                                h5("This data set is from bladder cancer data and contains Affymetrix gene expression microarray data. This data set is from the
+                                    bladderbatch package which must be installed to use this data example set"),
+                                h6("Reference: Leek J., et al. Tackling the widespread and critical impact of batch effects in high-throughput data. Nat Rev Genet 11, 733–739 (2010). https://doi.org/10.1038/nrg2825")
+                            ),
+                            conditionalPanel(condition = "input.exampleData == 'TbData'",
+                                h5("This data set is from a TB cohort study from Pondicherry India and contains RNA-seq counts data."),
+                                h6("Reference: VanValkenburg A, et al. Malnutrition leads to increased inflammation and expression of tuberculosis risk signatures in recently exposed household contacts of pulmonary tuberculosis. Front Immunol. 2022 Sep 28;13:1011166. doi: 10.3389/fimmu.2022.1011166")
+                            )),
+                        h4(strong("Data Preview")),
+                        DTOutput('counts_header'),
+                        textOutput('counts_dimensions'),
+                        DTOutput('se_counts'),
+                        textOutput('se_dimensions'),
+                        br()
+                    ),
                 tabPanel('Full Metadata',
                          h4(strong("Usage")),
                          h5("The table displayed here shows all metadata associated with the uploaded data. Please refer to this table to view sample condition information."),
@@ -160,4 +178,3 @@ tabPanel("Upload Data",
             )
         )
     )
-
